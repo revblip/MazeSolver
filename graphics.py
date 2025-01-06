c65b136eec8f724366265f5071517f41ecb0576c
revblip <>
2025-01-06 16:30:13 -0600

    separated main from window

---
+++ graphics.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Window Title")
+        self.canvas = Canvas(self.root, width=self.width, height=self.height, bg="yellow")
+        self.canvas.pack()
+        self.is_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+
